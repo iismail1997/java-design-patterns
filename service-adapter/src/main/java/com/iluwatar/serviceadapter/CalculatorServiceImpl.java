3f7e1fdddeb0427e8a1576c7b3ecb22f27d900e0
mahendher <mahendher.gk@gmail.com>
2021-11-30 19:43:29 -0500

    pmd results

--- service-adapter/src/main/java/com/iluwatar/serviceadapter/CalculatorServiceImpl.java
+++ service-adapter/src/main/java/com/iluwatar/serviceadapter/CalculatorServiceImpl.java
@@ -3,13 +3,13 @@ package com.iluwatar.serviceadapter;
 public class CalculatorServiceImpl implements CalculatorService {
 
     /* Funtion to add two integers*/
-    public int addIntegers(int A, int B) {
-        return A + B ;
+    public int addIntegers(int a, int b) {
+        return a + b ;
     }
 
     /* Funtion to divide two integers*/
-    public int divideIntegers(int A, int B){
+    public int divideIntegers(int a, int b){
         ServiceAdapter serviceAdapter = new ServiceAdapter();
-        return serviceAdapter.divideIntegers(A, B);
+        return serviceAdapter.divideIntegers(a, b);
     }
 }
--- service-adapter/src/main/java/com/iluwatar/serviceadapter/ServiceAdapter.java
+++ service-adapter/src/main/java/com/iluwatar/serviceadapter/ServiceAdapter.java
@@ -4,8 +4,8 @@ public class ServiceAdapter {
 
     DivideIntegerServiceRequest service = new DivideIntegerServiceRequest();
     /* Function that actually calls the external remote service to divide two integers*/
-    public int divideIntegers(int A, int B){
-        return A/B; // replace this with remote soap service utility call
+    public int divideIntegers(int a, int b){
+        return a/b; // replace this with remote soap service utility call
     }
 
 
