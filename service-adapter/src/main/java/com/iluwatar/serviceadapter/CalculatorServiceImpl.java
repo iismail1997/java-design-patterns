af7311e917dbebf0b72f1b17677b8790d1dbc44a
mahendher <mahendher.gk@gmail.com>
2021-12-09 21:48:12 -0500

    updated java docs

--- service-adapter/src/main/java/com/iluwatar/serviceadapter/CalculatorServiceImpl.java
+++ service-adapter/src/main/java/com/iluwatar/serviceadapter/CalculatorServiceImpl.java
@@ -1,14 +1,24 @@
 package com.iluwatar.serviceadapter;
 
+/**
+ * The type Calculator service.
+ */
 public class CalculatorServiceImpl implements CalculatorService {
-
-    /* Funtion to add two integers*/
     public long addIntegers(long a, long b) {
+        /**
+         * Method to add two integers
+         * @param a and b refers to the two integers that needs to be added
+         * @return addition result of the two integers
+         */
         return a + b ;
     }
 
-    /* Function to divide two integers using a service adapter*/
     public long divideIntegers(long a, long b) {
+        /**
+         * Method to divide two integers
+         * @param a and b refers to the two integers that needs to be divided
+         * @return division result of the two integers.
+         */
         ServiceAdapter serviceAdapter = new ServiceAdapter();
         return serviceAdapter.divideIntegers(a, b);
     }
